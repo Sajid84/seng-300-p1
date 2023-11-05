a39bbb62f053b9ed780789da4f66316854317bfd
Atique6 <muhammadatique3@gmail.com>
2023-11-04 22:40:47 -0600

    Add files via upload
    Name: Atique Muhammad, UCID: 30038650
    
    Added comments.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaCoin.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaCoin.java
@@ -11,18 +11,29 @@ import com.thelocalmarketplace.hardware.SelfCheckoutStation;
 public abstract class PayViaCoin extends Cart implements Sink<Coin> {
 	
 	private BigDecimal remainingAmountDue;
-	private Coin coin;
 	private BigDecimal coinInserted;
+	private Coin coin;
 	
 	public PayViaCoin(SelfCheckoutStation selfCheckoutStation) {
 		super(selfCheckoutStation);
 	}
 
-    public void PayViaCoin(BigDecimal cartTotal) {
+	//sets the initial amount that is due by the customer 
+    public void setAmountDue(BigDecimal cartTotal) {
         this.remainingAmountDue = cartTotal;
     }
       
-
+    /**
+	 * Processes the payment from the customer with coins only
+	 * After each insertion, calculates the amount remaining and presents the updated amount due
+	 * 
+	 * @throws DisabledException
+	 *             If the coin slot is currently disabled and cannot accept coins
+	 *              
+	 * @throws CashOverloadException
+	 *             If the coin slot is currently full and there is no space 
+	 *             to accept coins 
+	 */
     public void processCoinPayment() throws DisabledException, CashOverloadException {
     	
     	
@@ -32,7 +43,10 @@ public abstract class PayViaCoin extends Cart implements Sink<Coin> {
     		receive(coin);   		
     		coinInserted = coin.getValue();  
     		
-    		remainingAmountDue = remainingAmountDue.subtract(coinInserted);
+    		//Reduce the remaining amount due by the value of the inserted coin
+    		remainingAmountDue = remainingAmountDue.subtract(coinInserted);		
+    		
+    		//Present to the Customer the updated amount due
     		System.out.println("Remaining amount due: " + remainingAmountDue);    		
     	}
     	
